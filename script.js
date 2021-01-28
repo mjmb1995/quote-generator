cc9dfbf14b9a3e13880bd90a6988fa99b80c24d4
mjmb1995 <markjoshuabihag@gmail.com>
2021-01-28 21:16:22 +0800

    corrected js syntax error

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function removeLoadingSpinner() {
 
 // Get quote from API
 async function getQuote() {
-  showLoadingSpinner;
+  showLoadingSpinner();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
